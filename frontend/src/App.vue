b0f1776c61b37c70e41416e08cc60513308a126d
Saksham Sirohi <sakshamsirohi27@gmail.com>
2025-02-02 19:20:58 +0530

    navbar and footer seperated towards components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,9 @@
+<script setup>
+import navbar from './components/navbar.vue'
+import footer1 from './components/footer.vue'
+</script>
 <template>
+  <navbar />
   <router-view></router-view>
+  <footer1 />
 </template>

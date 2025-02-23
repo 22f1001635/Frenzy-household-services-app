6477a74d74079430ff572c4c4b752b0ee3a54d94
Saksham Sirohi <sakshamsirohi27@gmail.com>
2025-02-23 12:00:00 +0530

    Fixed profile picture path issue

--- backend/config.py
+++ backend/config.py
@@ -7,5 +7,6 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
-    UPLOAD_FOLDER = os.path.abspath(os.getenv('UPLOAD_FOLDER', 'instance/profile_pictures'))
+    UPLOAD_FOLDER = os.path.abspath(os.getenv('UPLOAD_FOLDER', 'instance/uploads'))
     ALLOWED_EXTENSIONS = set(os.getenv('ALLOWED_EXTENSIONS', 'png,jpg,jpeg').split(','))
+    PROFILE_PIC_FOLDER = os.path.join(UPLOAD_FOLDER, 'profile_pictures')
